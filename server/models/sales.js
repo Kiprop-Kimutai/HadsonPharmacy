70278d7f2f5350de8c11efe29570f229370bcd5e
jonah <jonah.kimutai@compulynx.com>
2018-08-22 14:11:06 +0300

    mongoose schema for sales

--- server/models/sales.js
+++ server/models/sales.js
@@ -9,7 +9,7 @@ var counterSchema = new Schema({
 
 var sales_counter = mongoose.model('sales_counter',counterSchema);
 
-var sales = new Schema({
+var salesChema = new Schema({
     id:{type:Number,unique:true},
     invoice_number:{type:String,unique:true},
     cashier:{type:String},
@@ -19,3 +19,23 @@ var sales = new Schema({
     balance:{type:Number},
     created_at:{type:Date}
 })
+
+salesSchema.pre('save',function(nex){
+    var doc = this;
+    sales_counter.findByIdAndUpdate({_id:'entityId'},{$inc:{seq:1}},{new:true,upsert:true}).then(function(count){
+        doc.id = count.seq;
+        var date = new Date();
+
+        if(!doc.created_at){
+            doc.created_at = date;
+        }
+        //doc.updated_at = date;
+        next();
+    }).catch(function(err){
+        console.error(err);
+        throw err;
+    });
+})
+
+var Sales = mongoose.model('Sales',salesChema);
+module.exports = Sales;
