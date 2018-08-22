0b2f0233583c02f2571eaded5f3933e13288f3b3
jonah <jonah.kimutai@compulynx.com>
2018-08-22 14:14:58 +0300

    mongoose schema for sales order

---
+++ server/models/sales_order.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var counterSchema = new Schema({
+    _id:{type:String,require:true},
+    seq:{type:Number,default:0}
+})
+
+var counter_schema = mongoose.model('counter_schema',counterSchema);
+
+var salesOrderSchema = new Schema({
+    id:{type:Number,unique:true},
+    invoice:{type:String},
+    product:{type:String},
+    quantity:{type:Number},
+    amount:{type:Number},
+    profit:{type:Number},
+    product_code:{type:String},
+    gen_name:{type:String},
+    name:{type:String},
+    price:{type:Number},
+    discount:{type:Number},
+    created_at:{type:Date}
+})
+
+salesOrderSchema.pre('save',function(next){
+    var doc = this;
+    counter_schema.findByIdAndUpdate({_id:'entityId'},{$inc:{seq:1}},{new:true,upsert:true}).then(function(count){
+        doc.id = count.seq;
+        var date = new Date();
+
+        if(!doc.created_at){
+            doc.created_at = date;
+        }
+        //doc.updated_at = date;
+        next();
+    }).catch(function(err){
+        console.error(err);
+        throw err;
+    });
+})
+
+var SalesOrder = mongoose.model('SalesOrder',salesOrderSchema);
