bba84eb6d03ee2bd17a85db31c976421c288d9f5
jonah <jonah.kimutai@compulynx.com>
2018-08-26 04:08:37 +0300

    #.ts model for products

--- src/app/models/products.ts
+++ src/app/models/products.ts
@@ -1,6 +1,6 @@
 export class Product {
     constructor(
-        public product_id:Number,
+        public product_id:number,
         public product_code:string,
         public gen_name:string,
         public product_name:string,
@@ -8,14 +8,14 @@ export class Product {
         public supplier:string,
         public units:string,
         public unit_quantity:string,
-        public unit_price:Number,
-        public actual_cost:Number,//(unit_price*units)
-        public discount:Number,
-        public unit_discount:Number,//(discount/units)
-        public cost:Number,//(actual_cost-discount)
-        public ordering_price:Number,//(actual_cost/original_quantity)
-        public selling_price:Number,//(percentage of selling price)
-        public profit:Number, //(ordering_price-selling_price)
+        public unit_price:number,
+        public actual_cost:number,//(unit_price*units)
+        public discount:number,
+        public unit_discount:number,//(discount/units)
+        public cost:number,//(actual_cost-discount)
+        public ordering_price:number,//(actual_cost/original_quantity)
+        public selling_price:number,//(percentage of selling price)
+        public profit:number, //(ordering_price-selling_price)
         public original_quantity:number,
         public qty_sold:number,
         public onhand_qty:number, //(original_quantity-qty_sold)
