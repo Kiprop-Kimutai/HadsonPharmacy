e93c7ada4815d82d050effd2129b14b11ea4a567
jonah <jonah.kimutai@compulynx.com>
2018-08-26 04:12:37 +0300

    #sales module component

--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -1,4 +1,4 @@
-import {Component,OnInit} from '@angular/core';
+import {Component,OnInit,Input,ViewChild} from '@angular/core';
 import {Sales} from '../models/sales';
 import {SalesOrder} from '../models/sales_order';
 import {Product} from '../models/products';
@@ -6,6 +6,7 @@ import {FormControl,FormGroup,Validators} from '@angular/forms';
 import {ProductsService} from '../products/products.service';
 import {MatTableDataSource,PageEvent} from '@angular/material';
 import {paginatorFunction} from '../common/PaginatorFunction';
+import {SalesOrderComponent} from './salesorder.component';
 @Component({
     selector:'sales-component',
     templateUrl:'./sales.component.html',
@@ -13,25 +14,25 @@ import {paginatorFunction} from '../common/PaginatorFunction';
     providers:[ProductsService]
 })
 export class SalesComponent implements OnInit{
+    @ViewChild(SalesOrderComponent) private salesOrderComponent:SalesOrderComponent
     salesFormGroup:FormGroup;
     SalesOrderFormGroup:FormGroup;
     products:Product[];
+    salesOrder:SalesOrder[] = new Array;
+    salesOrderCart:SalesOrder[] = new Array;
     productsCopy:Product[];
+    selectedProducts:Product[];
     paginatedProducts:Product[];
     showproductslist:Boolean = false;
+    showsalesTable:Boolean = false;
     pageIndex:number = 0;
     pageSize:number;
     dataLength:number;
     dataSource:MatTableDataSource<Product>;
-    salesOrderDataSource:MatTableDataSource<SalesOrder>;
-    salesorderdataLength:number;
-    productspageSize:number;
-    salesorderpageSize:number;
     productsdisplayedColumns:string[];
-    salesorderdisplayedColumns:string[];
-    salesorderpageIndex:number = 0;
+    name:String = "Jonah Hexx";
     constructor(private productsService:ProductsService){
-
+        this.createForms();
     }
     createForms(){
         this.salesFormGroup = new FormGroup({
@@ -47,7 +48,7 @@ export class SalesComponent implements OnInit{
         this.SalesOrderFormGroup = new FormGroup({
             invoice:new FormControl('',Validators.required),
             product:new FormControl('',Validators.required),
-            quantity:new FormControl('',Validators.required),
+            quantity:new FormControl(Validators.required),
             amount:new FormControl('',Validators.required),
             profit:new FormControl('',Validators.required),
             product_code:new FormControl('',Validators.required),
@@ -62,7 +63,7 @@ export class SalesComponent implements OnInit{
     fetchProducts():Product[]{
        
         this.productsService.fetchAllProducts().subscribe(data =>{this.products = data.response_message;this.productsCopy = data.response_message;console.log(data);this.paginatedProducts = data.response_message;this.dataLength = data.response_message.length;});
-        return this.products;
+        return this.productsCopy;
     }
     toggleProductList(){
         this.showproductslist = (this.showproductslist === false ?true:false);
@@ -70,6 +71,7 @@ export class SalesComponent implements OnInit{
     filterProducts(text:string){
         queryString = text;
         this.products = this.productsCopy.filter(this.filterProduct);
+       
     }
     filterProduct(product:Product){
         var patt = new RegExp(queryString,"i");
@@ -84,6 +86,35 @@ export class SalesComponent implements OnInit{
     paginateValues(pageSize:number,pageIndex:number){
         this.products = <Product[]>paginatorFunction(this.paginatedProducts,pageSize,pageIndex);
     }
+
+    loadSelectedItem(id:number){
+        console.log("Trying.....");
+        console.log(id);
+        selectedProductId = id;
+        this.showproductslist = false;
+        this.showsalesTable = true;
+        this.selectedProducts = this.products.filter(this.findProductById);
+        //this.showsalesTable = false;
+        this.salesOrderCart = this.castSelectedProductToSalesOrder(this.selectedProducts);
+        this.salesOrderComponent.setValues(this.salesOrderCart);
+    }
+
+    castSelectedProductToSalesOrder(products:Product[]):SalesOrder[]{
+        for(var i =0;i<products.length;i++){
+        
+            this.salesOrder.push(new SalesOrder(0,"",products[i].invoice_no,""+products[i].product_id,4,((products[i].selling_price)*4),
+                        (4*products[i].profit),products[i].product_code,products[i].gen_name,products[i].gen_name,products[i].selling_price,products[i].discount,new Date()));
+        }
+        console.log("*****************************>>");
+        console.log(this.salesOrder);
+        return this.salesOrder;
+        
+    }
+
+    findProductById(product){
+        return product.id == selectedProductId;
+    }
+
     ngOnInit(){
         //this.fetchProducts();
         this.dataSource = new MatTableDataSource(this.fetchProducts());
@@ -92,4 +123,64 @@ export class SalesComponent implements OnInit{
     }
 }
 
+@Component({
+    selector:'sales-order',
+    templateUrl:'./salesorder.component.html',
+    styleUrls:['./salesorder.component.css']
+})
+export class SalesOrderComponent1 implements OnInit{
+    salesOrder:SalesOrder[];
+    @Input() set current_sale_order(current_sale_order:SalesOrder[]){
+            this.salesOrder = current_sale_order;
+            console.log("****************");
+            console.log(this.salesOrder)
+    }
+    get current_sale_order(){
+        return this.salesOrder;
+    }
+    @Input()title:String;
+    salesOrderCopy:SalesOrder[];
+    salesOrderProper:SalesOrder[] = new Array;
+    salesOrderFormGroup:FormGroup;
+    pageIndex:number = 0;
+    pageSize:number;
+    dataLength:number;
+    dataSource:MatTableDataSource<SalesOrder>;
+    displayedColumns:string[];
+    showTable:Boolean = false;
+
+    constructor(){
+        this.createFormGroup();
+    }
+    createFormGroup(){
+        this.salesOrderFormGroup = new FormGroup({
+        })
+    }
+
+    paginateValues(pageSize:number,pageIndex:number){
+        this.salesOrder = <SalesOrder[]>paginatorFunction(this.salesOrderCopy,pageSize,pageIndex);
+    }
+
+    getSalesOrderItems():SalesOrder[]{
+        for(let i =0;i<this.current_sale_order.length;i++){
+            let order:SalesOrder = this.current_sale_order[i];            
+            this.salesOrderProper.push(order);
+        }
+        console.log(this.salesOrderProper);
+        return this.salesOrderProper;
+    }
+    ngOnInit(){
+        console.log("A new entrant");
+        console.log(this.salesOrder);
+        console.log(this.salesOrder[0].getAmount());
+        this.displayedColumns = ["invoice_no","name","gen_name","product_code","price","quantity","amount","profit","Action"];
+        this.pageSize = 10;
+        this.dataLength = this.salesOrder.length;
+        this.dataSource = new MatTableDataSource(this.getSalesOrderItems());
+        //this.salesOrderCopy = this.salesOrder;
+        this.getSalesOrderItems();
+    }
+}
+
 var queryString = "";
+var selectedProductId:number;
