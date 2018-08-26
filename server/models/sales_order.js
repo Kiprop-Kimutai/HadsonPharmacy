d58abced57b447f647351d2ca8e9a515e16fd69c
jonah <jonah.kimutai@compulynx.com>
2018-08-26 04:09:42 +0300

    #sales_order mongoose schema

--- server/models/sales_order.js
+++ server/models/sales_order.js
@@ -10,6 +10,7 @@ var counter_schema = mongoose.model('counter_schema',counterSchema);
 
 var salesOrderSchema = new Schema({
     id:{type:Number,unique:true},
+    receipt_number:{type:String,unique:true},
     invoice:{type:String},
     product:{type:String},
     quantity:{type:Number},
@@ -28,6 +29,7 @@ salesOrderSchema.pre('save',function(next){
     counter_schema.findByIdAndUpdate({_id:'entityId'},{$inc:{seq:1}},{new:true,upsert:true}).then(function(count){
         doc.id = count.seq;
         var date = new Date();
+        doc.receipt_number = count.seq + date;
 
         if(!doc.created_at){
             doc.created_at = date;
