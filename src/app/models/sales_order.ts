3be082885e59a47608a38721ca9c3ed599809b4b
jonah <jonah.kimutai@compulynx.com>
2018-08-26 04:07:35 +0300

    #.ts model for sales_order

--- src/app/models/sales_order.ts
+++ src/app/models/sales_order.ts
@@ -1,7 +1,8 @@
 export class SalesOrder{
     constructor(
         public id:Number,
-        public invoice:string,
+        public receipt_number:string,
+        public invoice_no:string,
         public product:string,//product id
         public quantity:Number,
         public amount:Number,
@@ -13,4 +14,44 @@ export class SalesOrder{
         public discount:Number,
         public created:Date
     ){}
+
+    public getId(){
+        return this.id;
+    }
+    public getReceipt_number(){
+        return this.receipt_number;
+    }
+    public getInvoiceNo(){
+        return this.invoice_no;
+    }
+    public getProduct(){
+        return this.product;
+    }
+    public getQuantity(){
+        return this.quantity;
+    }
+    public getAmount(){
+        return this.amount;
+    }
+    public getProfit(){
+        return this.profit;
+    }
+    public getProductCode(){
+        return this.product_code;
+    }
+    public getGenName(){
+        return this.gen_name;
+    }
+    public getName(){
+        return this.name;
+    }
+    public getPrice(){
+        return this.price;
+    }
+    public getDiscount(){
+        return this.discount;
+    }
+    public getCreatedAt(){
+        return this.created;
+    }
 }
