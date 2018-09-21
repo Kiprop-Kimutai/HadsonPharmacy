67de1a9233b28c4d60a769f94ac895de1fe01884
jonah <jonah.kimutai@compulynx.com>
2018-09-21 23:38:38 +0300

    #product catalogue mongoose schema

---
+++ server/models/product_catalogue.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var product_catalogue_schema = new Schema({
+    code:{type:String,unique:true},
+    generic_name:{type:String,unique:true},
+    name:{type:String,unique:true},
+    created_at:Date,
+    updated_at:Date
+})
+
+//set date before creation and update
+product_catalogue_schema.pre('save',function(next,err){
+    var doc = this;
+    var date = new Date();
+    if(!doc.created_at){
+        doc.created_at = date;
+    }
+    doc.updated_at = date;
+    next();
+    if(err){
+        //console.error(err);
+        //throw err;
+    }
+})
+
+var ProductCatalogue = mongoose.model('ProductCatalogue',product_catalogue_schema);
+module.exports = ProductCatalogue;
+
+
