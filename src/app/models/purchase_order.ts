b1f7e15f44acaccd825db653b837ceef2c191da9
jonah <jonah.kimutai@compulynx.com>
2018-09-21 23:55:31 +0300

    #TS purchase order model

--- src/app/models/purchase_order.ts
+++ src/app/models/purchase_order.ts
@@ -2,13 +2,15 @@ import {Product} from './products';
 export class PurchaseOrder{
     constructor(
         public id:number,
+        public invoice_no:string,
         public ordered_by:string,
         public description:string,
         public supplier:string,
         public po_number:string,
-        public Date:Date,
+        public date:Date,
         public raised_by:string,
-        public product:Product[],
+        public products:Product[],
+        public status:string, //any of pending,cancelled,delivered
         public created_at:Date,
         public updated_at:Date
     ){}
