cfcbcacfdc20b46cf195933b88c1adbb08dd0c91
jonah <jonah.kimutai@compulynx.com>
2018-09-21 23:48:59 +0300

    #purchase order mongoose schema

--- server/models/purchase_order.js
+++ server/models/purchase_order.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/pbu_tms",{useNewUrlParser:true});
 var Schema = mongoose.Schema;
-import {ProductModel} from './product-model';
 
 
 //create auto-increment counter
@@ -8,25 +8,27 @@ var counterSchema  = new Schema({
     _id:{type:String,require:true},
     seq:{type:Number,default:0}
 })
-var Purchase_OrderSchema = mongoose.model("Purchase_OrderSchema",counterSchema);
+var Purchase_OrderCounterSchema = mongoose.model("Purchase_OrderSchema",counterSchema);
 
 var Purchase_OrderSchema = new Schema({
-    id:{type:Number,unique:true},
-    ordered_by:{type:String},
-    description:{type:String},
-    supplier:{type:String},
-    po_number:{type:String,unique:true},
-    Date:{type:Date},
-    raised_by:{type:String},
-    product:{type:ProductModel},
-    created_at:{type:Date},
-    updated_at:{type:Date}
+    id:Number,
+    invoice_no:String,
+    status:String,
+    ordered_by:String,
+    description:String,
+    supplier:String,
+    po_number:String,
+    date:Date,
+    raised_by:String,
+    products:Array,
+    created_at:Date,
+    updated_at:Date
 })
 
 Purchase_OrderSchema.pre('save',function(next){
     var doc = this;
-    counterSchema.findByIdAndUpdate({_id:'entityId'},{$inc:{seq:1}},{new:true,upsert:true}).then(function(count){
-        doc.product_id = count.seq;
+    Purchase_OrderCounterSchema.findByIdAndUpdate({_id:'entityId'},{$inc:{seq:1}},{new:true,upsert:true}).then(function(count){
+        doc.id = count.seq;
         var date = new Date();
 
         if(!doc.created_at){
