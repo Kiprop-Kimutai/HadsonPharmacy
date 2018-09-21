b3a02d5ea81d35ca4b2fbcb1245b4a597c932365
jonah <jonah.kimutai@compulynx.com>
2018-09-21 23:49:49 +0300

    #Added LPO routes to API

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,15 @@ var app = express();
 var FirmwareRoutes = require('./firmware-routes');
 var ProductRoutes = require('./product-routes');
 const DeviceRoutes = require('./device-routes');
+var PurchaseOrderRoutes = require('./purchase_order_routes');
+const ProductCataloguRoutes = require('../routes/product_catalogue-routes');
 app.get('/',(requestAnimationFrame,res,next)=>{
     res.send("ok...");
 })
 app.use('/firmware_routes',FirmwareRoutes);
 app.use('/device_routes',DeviceRoutes);
 app.use('/products',ProductRoutes);
+app.use('/lpo',PurchaseOrderRoutes);
+app.use('/catalogue',ProductCataloguRoutes);
 
 module.exports = app;
