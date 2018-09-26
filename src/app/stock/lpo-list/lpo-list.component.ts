dd0b4c5da3b15facc55e84954914d0af747b5ddc
jonah <jonah.kimutai@compulynx.com>
2018-09-26 14:29:15 +0300

    #lpo-list component

--- src/app/stock/lpo-list/lpo-list.component.ts
+++ src/app/stock/lpo-list/lpo-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {LPOService} from '../lpo/lpo.service';
+import {StockService} from '../stock.service';
 import {PurchaseOrder} from '../../models/purchase_order';
 import {MatTableDataSource,PageEvent} from '@angular/material';
 import {paginatorFunction} from '../../common/PaginatorFunction';
 import { Product } from '../../models/products';
 import { resolve } from 'url';
+import {Router} from  '@angular/router';
+import {FormGroup,FormArray,FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-lpo-list',
   templateUrl: './lpo-list.component.html',
@@ -19,14 +21,14 @@ export class LpoListComponent implements OnInit {
   dataLength:number;
   columnsToDisplay:string[];
   dataSource:MatTableDataSource<PurchaseOrder>;
-  constructor(private lpoService:LPOService) {
+  constructor(private stockservice:StockService,private router:Router) {
     this.columnsToDisplay = ["po_number","raised_by","supplier","created_at","status"];
     this.dataSource = new MatTableDataSource(this.pendingLPos);
    }
-
+   //function fetches all LPOs with status "pending"
    fetchAllPendingLPOs(){
     new Promise((resolve,reject) =>{
-      this.lpoService.fetchAllPendingLPOs().subscribe(responselpos =>{
+      this.stockservice.fetchAllPendingLPOs().subscribe(responselpos =>{
         delete responselpos.response_message.__v;
         delete responselpos.response_message._id;
         this.pendingLPos = responselpos.response_message;
@@ -39,18 +41,9 @@ export class LpoListComponent implements OnInit {
       console.log(this.pendingLPos);
       this.dataSource = new MatTableDataSource(this.pendingLPos);
     })
-    //  this.lpoService.fetchAllPendingLPOs().subscribe(pendinglpos =>{this.pendingLPos = pendinglpos.response_message;this.pendingLPOsCopy = pendinglpos.response_message;console.log(pendinglpos);console.log("--->"+this.pendingLPos)});
-    // //this.dataLength = this.pendingLPos.length;
-    // this.dataLength = 10;
-    // console.log("----------");
-    // console.log(this.pendingLPos);
-    // for(let key in this.pendingLPos){
-    //   console.log("....");
-    //   console.log(this.pendingLPos[key]);
-    // }
-    // return this.pendingLPos;
   }
 
+  //custome filter function for MatTableDataSource
   filterLPOs(text:string){
     queryString = text;
     this.pendingLPos = this.pendingLPOsCopy.filter(this.filterLPO);
@@ -64,6 +57,10 @@ export class LpoListComponent implements OnInit {
   paginateValues(pageSize:number,pageIndex:number){
     this.pendingLPos = <PurchaseOrder[]>paginatorFunction(this.pendingLPOsCopy,pageSize,pageIndex);
 
+  }
+  //function navigates to selected LPO item
+  navigateToLPOItem(id:number){
+    this.router.navigate(['/layout/stock-management/lpolist',id]);
   }
   ngOnInit() {
     this.fetchAllPendingLPOs();
