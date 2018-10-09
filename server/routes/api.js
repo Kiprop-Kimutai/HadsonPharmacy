dc806492ae0970b50a90d2615df52431cb713c7c
jonah <jonah.kimutai@compulynx.com>
2018-10-10 00:10:32 +0300

    #added sales routes to API

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,6 +5,7 @@ var ProductRoutes = require('./product-routes');
 const DeviceRoutes = require('./device-routes');
 var PurchaseOrderRoutes = require('./purchase_order_routes');
 const ProductCataloguRoutes = require('../routes/product_catalogue-routes');
+const SalesRoutes = require('../routes/sales-routes');
 app.get('/',(requestAnimationFrame,res,next)=>{
     res.send("ok...");
 })
@@ -13,5 +14,5 @@ app.use('/device_routes',DeviceRoutes);
 app.use('/products',ProductRoutes);
 app.use('/lpo',PurchaseOrderRoutes);
 app.use('/catalogue',ProductCataloguRoutes);
-
+app.use('/sales',SalesRoutes);
 module.exports = app;
