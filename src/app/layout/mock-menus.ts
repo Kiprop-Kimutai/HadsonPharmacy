c5b61573c1fd1b2eb1b09e6c4f14605dd748fbdd
jonah <jonah.kimutai@compulynx.com>
2018-10-10 13:48:58 +0300

    #standard menus

--- src/app/layout/mock-menus.ts
+++ src/app/layout/mock-menus.ts
@@ -10,8 +10,8 @@ export const MENUS = [
       new Menu('customers','Customers','customers','link'),
       //new Menu('suppliers','Suppliers','sup','link'),
       new Menu('reports','Reports','dashboard','parent',[new SubMenus('salesreport','Sales Report','link'),new SubMenus('stockreport','Stock Report','link')]),
-      new Menu('devices',"Devices",'devices','parent',[new SubMenus('device-list','Device List','link'),new SubMenus('device_firmware','Manage Firmware','link')]),
-      new Menu('firmware','Firmware','test','parent',[new SubMenus('saral-firmware','Saral Info','link'),new SubMenus('newpos-firmware','NewPos 9220','link')]),
+      //new Menu('devices',"Devices",'devices','parent',[new SubMenus('device-list','Device List','link'),new SubMenus('device_firmware','Manage Firmware','link')]),
+      //new Menu('firmware','Firmware','test','parent',[new SubMenus('saral-firmware','Saral Info','link'),new SubMenus('newpos-firmware','NewPos 9220','link')]),
       new Menu('clients','Clients','client','parent',[new SubMenus('wfp','WFP','link'),new SubMenus('pbu','Postbank UG','link'),new SubMenus('tbc','TBC','link')])
 
 ]
