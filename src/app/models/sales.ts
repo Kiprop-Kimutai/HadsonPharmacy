5018d4ebe40f14833b0717c27454bba6d16d3ad4
jonah <jonah.kimutai@compulynx.com>
2018-10-10 13:14:31 +0300

    #added cash to object

--- src/app/models/sales.ts
+++ src/app/models/sales.ts
@@ -3,10 +3,18 @@ export class Sales{
         public id:number,
         public receipt_number:string,
         public cashier:string,
+        public cash:Number,//tendered amount
         public amount:Number,
         public profit:Number,
         public name:string,
         public balance:Number,
         public created_at:Date,
     ){}
+
+    public  setCash(cash:Number){
+        this.cash= cash;
+    }
+    public  getCash():Number{
+        return this.cash;
+    }
 }
