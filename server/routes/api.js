d965e712635f2b21f73eb16bb7ef87cf27722f2b
jonah <jonah.kimutai@compulynx.com>
2018-11-05 11:43:03 +0300

    #

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,18 +1,42 @@
 var express = require('express');
 var app = express();    
-var FirmwareRoutes = require('./firmware-routes');
+var passport = require('passport');
+var jwt = require('express-jwt');
+var router = express.Router();
+require('../models/db');
+require('../config/passport');
+var ctrlAuth = require('../controllers/authentication');
+var ctrlProfile = require('../controllers/profile');
+
+var auth = jwt({
+    secret:'MY_SECRET',
+    userProperty:'payload'
+})
+
+
+app.use(passport.initialize());
 var ProductRoutes = require('./product-routes');
 const DeviceRoutes = require('./device-routes');
 var PurchaseOrderRoutes = require('./purchase_order_routes');
 const ProductCataloguRoutes = require('../routes/product_catalogue-routes');
 const SalesRoutes = require('../routes/sales-routes');
-app.get('/',(requestAnimationFrame,res,next)=>{
+var test = require('./test');
+var print = require('./print');
+
+
+app.get('/',(req,res,next)=>{
     res.send("ok...");
 })
-app.use('/firmware_routes',FirmwareRoutes);
+//profile
+router.get('/profile',auth,ctrlProfile.profileRead);
+//authentication
+app.post('/login',ctrlAuth.login);
+app.post('/register',ctrlAuth.register);
 app.use('/device_routes',DeviceRoutes);
 app.use('/products',ProductRoutes);
 app.use('/lpo',PurchaseOrderRoutes);
 app.use('/catalogue',ProductCataloguRoutes);
 app.use('/sales',SalesRoutes);
+app.use('/test',test);
+app.use('/print',print);
 module.exports = app;
